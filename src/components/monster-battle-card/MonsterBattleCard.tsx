72ea428f7052e26c965505dbe77c2cae6ce300ec
carlosavellar <carlos.avellar@gmail.com>
2023-09-09 18:10:34 -0300

    Monster power props

--- src/components/monster-battle-card/MonsterBattleCard.tsx
+++ src/components/monster-battle-card/MonsterBattleCard.tsx
@@ -1,17 +1,36 @@
-import { Monster } from "../../models/interfaces/monster.interface"
-import { BattleMonsterCard, BattleMonsterTitle } from "./MonsterBattleCard.styled"
+import { Monster } from '../../models/interfaces/monster.interface';
+import {
+  BattleMonsterCard,
+  BattleMonsterTitle,
+} from './MonsterBattleCard.styled';
 
 type MonsterCardProps = {
-    monster?: Monster | null
-    title?: string
-}
+  monster?: Monster | null;
+  title?: string;
+  attack: number;
+  defense: number;
+  hp: number;
+  speed: number;
+};
 
-const MonsterBattleCard: React.FC<MonsterCardProps> = ({ title }) => {
-    return (
-        <BattleMonsterCard centralized>
-            <BattleMonsterTitle>{title!}</BattleMonsterTitle>
-        </BattleMonsterCard>
-    )
-}
+const MonsterBattleCard: React.FC<MonsterCardProps> = ({
+  title,
+  attack,
+  defense,
+  hp,
+  speed,
+}) => {
+  return (
+    <BattleMonsterCard centralized>
+      <BattleMonsterTitle>
+        {title!}
+        <div>{attack!}</div>
+        <div>{defense!}</div>
+        <div>{hp!}</div>
+        <div>{speed!}</div>
+      </BattleMonsterTitle>
+    </BattleMonsterCard>
+  );
+};
 
-export { MonsterBattleCard }
+export { MonsterBattleCard };
--- src/pages/battle-of-monsters/BattleOfMonsters.tsx
+++ src/pages/battle-of-monsters/BattleOfMonsters.tsx
@@ -1,40 +1,62 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { useAppDispatch } from "../../app/hooks"
-import { MonsterBattleCard } from "../../components/monster-battle-card/MonsterBattleCard"
-import { MonstersList } from "../../components/monsters-list/MonstersList"
-import { Title } from "../../components/title/Title"
-import { fetchMonstersData } from "../../reducers/monsters/monsters.actions"
-import { selectMonsters, selectSelectedMonster } from "../../reducers/monsters/monsters.selectors"
-import { BattleSection, PageContainer, StartBattleButton } from "./BattleOfMonsters.styled"
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from '../../app/hooks';
+import { MonsterBattleCard } from '../../components/monster-battle-card/MonsterBattleCard';
+import { MonstersList } from '../../components/monsters-list/MonstersList';
+import { Title } from '../../components/title/Title';
+import { fetchMonstersData } from '../../reducers/monsters/monsters.actions';
+import {
+  selectMonsters,
+  selectSelectedMonster,
+} from '../../reducers/monsters/monsters.selectors';
+import {
+  BattleSection,
+  PageContainer,
+  StartBattleButton,
+} from './BattleOfMonsters.styled';
 
 const BattleOfMonsters = () => {
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-    const monsters = useSelector(selectMonsters)
-    const selectedMonster = useSelector(selectSelectedMonster)
+  const monsters = useSelector(selectMonsters);
+  const selectedMonster = useSelector(selectSelectedMonster);
 
-    useEffect(() => {
-        dispatch(fetchMonstersData())
-    }, []);
+  useEffect(() => {
+    dispatch(fetchMonstersData());
+  }, []);
 
-    const handleStartBattleClick = () => {
-        // Fight!
-    }
+  const handleStartBattleClick = () => {
+    // Fight!
+  };
 
-    return (
-        <PageContainer>
-            <Title>Battle of Monsters</Title>
+  return (
+    <PageContainer>
+      <Title>Battle of Monsters</Title>
 
-            <MonstersList monsters={monsters} />
+      <MonstersList monsters={monsters} />
 
-            <BattleSection>
-                <MonsterBattleCard title={selectedMonster?.name || "Player"}></MonsterBattleCard>
-                <StartBattleButton data-testid="start-battle-button"  disabled={selectedMonster === null} onClick={handleStartBattleClick}>Start Battle</StartBattleButton>
-                <MonsterBattleCard title="Computer"></MonsterBattleCard>
-            </BattleSection>
-        </PageContainer>
-    )
-}
+      <BattleSection>
+        <MonsterBattleCard
+          title={selectedMonster?.name || 'Player'}
+          attack={selectedMonster?.attack || 0}
+          defense={selectedMonster?.defense || 0}
+          hp={selectedMonster?.hp || 0}
+          speed={selectedMonster?.speed || 0}></MonsterBattleCard>
+        <StartBattleButton
+          data-testid="start-battle-button"
+          disabled={selectedMonster === null}
+          onClick={handleStartBattleClick}>
+          Start Battle
+        </StartBattleButton>
+        <MonsterBattleCard
+          title="Computer"
+          attack={0}
+          defense={0}
+          hp={0}
+          speed={0}></MonsterBattleCard>
+      </BattleSection>
+    </PageContainer>
+  );
+};
 
-export { BattleOfMonsters }
+export { BattleOfMonsters };
